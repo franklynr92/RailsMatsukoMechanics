35c165cf5bbe72cc1e2fed4e6004e5878243fbfc
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-08 00:00:38 -0400

    signing user in and showing them there vehicles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,20 @@ class UsersController < ApplicationController
 
     #signup
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to vehicles_path
+        else
+            render :new
+        end
     end
     
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
     
 end
