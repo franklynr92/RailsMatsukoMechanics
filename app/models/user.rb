1616aa03d9884e5fc4528e9a5e9eccebf26619a9
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-08 09:34:16 -0400

    add relationships between vehicle and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :vehicles
 
     validates :user_name, :email, :name, presence: true
     validates :user_name, :email, uniqueness: true
