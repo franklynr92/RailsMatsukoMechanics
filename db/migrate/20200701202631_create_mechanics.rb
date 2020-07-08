21f55d3f2ca72dd7f494225e5f7ccdea0e6e0dd5
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-08 13:41:31 -0400

    updated mechanic table

--- db/migrate/20200701202631_create_mechanics.rb
+++ db/migrate/20200701202631_create_mechanics.rb
@@ -2,7 +2,7 @@ class CreateMechanics < ActiveRecord::Migration[6.0]
   def change
     create_table :mechanics do |t|
       t.string :name
-      t.string :wizard_name
+      t.string :mechanic_name
       t.string :password_digest
       t.string :email
       t.timestamps
