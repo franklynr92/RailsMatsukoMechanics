e5d0dd3526986c2eab074a3057ae33d1f2ec7332
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-08 14:52:05 -0400

    changed relationship to proper naming convention

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,5 @@
 class Vehicle < ApplicationRecord
     belongs_to :user #needs user_id to save to database with belongs_to
     has_many :issues
-    has_many :mechanics through :issues
+    has_many :mechanics, through: :issues
 end
