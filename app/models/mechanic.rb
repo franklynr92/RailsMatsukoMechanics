394f574e07dbce54d7b957fcd8ca4bf408f9c137
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-08 14:51:58 -0400

    changed relationship to proper naming convention

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,7 @@
 class Mechanic < ApplicationRecord
+    has_secure_password
     has_many :issues
-    has_many :vehicles through: issues
+    has_many :vehicles, through: :issues
+    validates :mechanic_name, :email, :name, presence: true
+    validates :mechanic_name, :email, uniqueness: true
 end
