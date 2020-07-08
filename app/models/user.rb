c27240928f0043713a8ecac2ebdb0ec092971f18
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-08 14:52:01 -0400

    changed relationship to proper naming convention

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :vehicles
-    has_many :issues through :vehicles
+    has_many :issues, through: :vehicles
 
     validates :user_name, :email, :name, presence: true
     validates :user_name, :email, uniqueness: true
