b07f8b26dc56d8b8be5c857bda74bd88ecbddf4d
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-09 09:42:57 -0400

    added comments

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -4,4 +4,7 @@ class Mechanic < ApplicationRecord
     has_many :vehicles, through: :issues
     validates :mechanic_name, :email, :name, presence: true
     validates :mechanic_name, :email, uniqueness: true
+    #make it where only the mechanic can resolve the issue has a mechanic or possibly
+    #a user can resolve like github, learn, quora
+    
 end
