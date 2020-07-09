1770001ede2f8581c64420de70e50355a4ab38af
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-08 22:38:16 -0400

    adding a current mechanic helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,7 @@ helper_method :current_user
       @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
+    def current_mechanic
+      @current_mechanic ||= Mechanic.find_by_id(session[:mechanic_id]) if session[:user_id]
+    end
 end
