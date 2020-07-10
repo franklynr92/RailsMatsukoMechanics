407effe1697128a4c78b8f52d1d7bb30c6140f20
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-10 01:00:37 -0400

    added action routes to the views

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,4 +1,36 @@
 class IssuesController < ApplicationController
     def index
+      @issue = Issue.all
     end
+    
+    def new
+      @issue = Issue.new
+    end
+
+    def create
+        @issue = Issue.new(issue_params)
+       
+       #@issue.user_id = session[:user_id]
+     if @issue.save(validate: false)
+        
+      redirect_to issues_path(@issue)  
+     else
+        byebug
+        render :new
+     end
+
+    end
+
+    def show
+        @issue = Issue.find_by(id: params[:id])
+    end
+
+
+    private
+
+    def issue_params
+      params.require(:issue).permit(:description_of_issue)
+    end
+
+
 end
