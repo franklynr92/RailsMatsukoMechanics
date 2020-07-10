037ee7fd65f32726af208c2e61d0856d7c9999a5
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-10 01:01:13 -0400

    added method/action to sign up mechanic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,11 +11,21 @@ class SessionsController < ApplicationController
         @user = User.find_by(user_name: params[:user][:user_name])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-            redirect_to '/profile'
+            redirect_to user_path(@user.id)
         else
             redirect_to '/login' #new
         end
+        elsif params[:mechanic]
+            @mechanic = Mechanic.find_by(mechanic_name: params[:mechanic][:mechanic_name])
+            if @mechanic && @mechanic.authenticate(params[:mechanic][:password])
+                session[:mechanic_id] = @mechanic.id
+                redirect_to admin_show_mechanic_path(@mechanic.id)
+            else
+                redirect_to admin_login_path
+            end
+        end
     end
+    
     
     def home
     end
