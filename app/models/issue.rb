ebb127c5156ee49c9f81bd2ee4566085d6b25c20
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-10 10:26:44 -0400

    gave mechanic optional attribute, to assign later

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,8 +1,8 @@
 class Issue < ApplicationRecord
-    belongs_to :mechanic
+    belongs_to :mechanic, class_name: 'Mechanic', optional: true
     belongs_to :vehicle
 
-    #validates :description_of_issue, presence: true
+    validates :description_of_issue, presence: true
 
 =begin
 needs an method with sql that picks the object where the issue.vehicle_id = vehicle.id
