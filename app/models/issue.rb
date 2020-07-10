62855f27f00eb11a8680aa0262c699805a8035ed
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-10 02:50:46 -0400

    add comments about sql

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,5 +2,10 @@ class Issue < ApplicationRecord
     belongs_to :mechanic
     belongs_to :vehicle
 
-    validates :description_of_issue, presence: true
+    #validates :description_of_issue, presence: true
+
+=begin
+needs an method with sql that picks the object where the issue.vehicle_id = vehicle.id
+i want the vehicle connected to that issue and i want the vehicle name
+=end
 end
