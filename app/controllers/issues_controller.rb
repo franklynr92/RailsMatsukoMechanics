7c19735f176dbbef2c4ff5b1b41fe950a9402fa4
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-13 20:11:56 -0400

    updated controller to check if user is logged in

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,28 +1,44 @@
 class IssuesController < ApplicationController
     def index
-      @issue = Issue.all
+      if logged_in?
+        @issue = Issue.all
+      else
+        flash[:notice]= "Please log in"
+        redirect_to '/login' 
+      end
     end
     
     def new
-      @issue = Issue.new
+      if logged_in?
+        @issue = Issue.new
+      else
+        flash[:notice]= "Please log in"
+        redirect_to '/login' 
+      end
     end
 
     def create
+      if logged_in?
         @issue = Issue.new(issue_params)
-       
-       #@issue.user_id = session[:user_id]
-     if @issue.save
-        
-      redirect_to issues_path(@issue)  
-     else
-        
-        render :show
+        if @issue.save
+          redirect_to issues_path(@issue)  
+        else
+          flash[:notice]= "Issue didn't save"
+          render :show
+        end
+      else
+        flash[:notice]= "Please log in"
+        redirect_to '/login' 
      end
-
     end
 
     def show
+      if logged_in?
         @issue = Issue.find_by(id: params[:id])
+      else
+        flash[:notice] = "Please log in"
+        redirect_to '/login'
+      end
     end
 
 
