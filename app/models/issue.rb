10df0c20f0fbe9b595cece92adbe8f0f65bf4f27
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-15 17:21:28 -0400

    added validations to user and removed comments

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,10 +1,4 @@
 class Issue < ApplicationRecord
     belongs_to :vehicle
-    validates :description_of_issue, presence: true
-
-=begin
-needs an method with sql that picks the object where the issue.vehicle_id = vehicle.id
-i want the vehicle connected to that issue and i want the vehicle name
-belongs_to :mechanic, class_name: 'Mechanic', optional: true
-=end
+    validates :vehicle_id, :description_of_issue, :date, :title, presence: true
 end
