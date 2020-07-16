85e426b24f497d75666e79e92b8aecfadd5b016b
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-16 02:05:28 -0400

    editing validations

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,7 +1,7 @@
 class Vehicle < ApplicationRecord
     belongs_to :user #needs user_id to save to database with belongs_to
     has_many :issues
-    validates :vehicle_name, :model, :make, :color, :year, :wheel_size, presence: true => {message: 'Please Fill In'}
+    validates :vehicle_name, :model, :make, :color, :year, :wheel_size, presence: {true => message: 'Please Fill In'}
     validates :mileage, numericality: {only_integer: true}
     validates :wheel_size, numericality: { less_than_or_equal_to: 30 }
     validates_numericality_of :year, less_than_or_equal_to: Time.now.year + 1
