9c29c99371a30491a113c589add0887e585abef6
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-21 10:13:36 -0400

    removing comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-    #loading the signup form
+    
     def new
         @user = User.new
     end
 
 
-    #signup
+    
     def create
         @user = User.new(user_params)
         if @user.save
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find_by(id: session[:user_id]) #it is the first time it is going to the profile
+        @user = User.find_by(id: session[:user_id]) 
         if @user
          render :show
         else 
@@ -27,10 +27,7 @@ class UsersController < ApplicationController
         end
     end
     
-    #def show
-     #   @user = User.find_by(id: params[:id]) #it is getting the params
-    #end
-    
+  
 
     private
 
